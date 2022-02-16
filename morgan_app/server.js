f7735eb00c3e0feca802849b06e9ebb688d8f4a7
Saheb Dhara <sahebdhara961@gmail.com>
2022-02-17 00:06:03 +0530

    last commit of morgan app

--- morgan_app/server.js
+++ morgan_app/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const morgan = require('morgan')// you have to install the module
 const {v4:uuidv4}  = require('uuid')
+const fs = require('fs')
+const path = require('path')
 
 
 const app = express()
@@ -12,15 +14,22 @@ morgan.token('id',function getid (req){
 return req.id
 })
 
-// how to use tken in morgan 
+// how to create token in morgan 
 morgan.token('param',function(req,res,param){
-    return "userToken";
+    // const time =  + new Date()
+    // return time;
+    return "username";
 })
 
 
 app.use(assigned);
 // use of morgan method
-app.use(morgan(':id :method :status :url"HTTP/:http-version"'))// know more in morgan git hub page 
+
+let thisfile = fs.createWriteStream(path.join(__dirname,"access.log"),{flags:'a'})
+
+
+app.use(morgan(':id :method :param :status :url"HTTP/:http-version"'))// know more in morgan git hub page 
+app.use(morgan(':param',{stream: thisfile}))
 
 // rout 
 app.get('/',(req,res)=>{
@@ -37,4 +46,9 @@ app.listen(port,()=>{
     console.log(`The server is running on the port ${port}`)
     
 })
+
+// using this techinque you can get the request of the user 
+// you can get all the user information of the user request 
+// you can store the information in your database as well
+
 
